f83f99b9541f877e7d89724198164bffb8587b7d
adminimusRU <adminimus.reg@gmail.com>
2018-08-25 17:23:20 +0500

    common files from tournament

---
+++ ow_defines.js
@@ -0,0 +1,52 @@
+var class_names = [
+	"tank",
+	"dps",
+	"support"
+];
+
+var hero_classes = {
+	reaper:		"dps",
+	pharah:		"dps",
+	soldier76:	"dps",
+	genji:		"dps",
+	tracer:		"dps",
+	sombra:		"dps",
+	mccree:		"dps",
+	doomfist:	"dps",
+	
+	torbjorn:	"dps",
+	hanzo:		"dps",
+	mei:		"dps",
+	bastion:	"dps",
+	widowmaker:	"dps",
+	junkrat:	"dps",
+	
+	symmetra:	"dps",
+	
+	mercy:		"support",
+	ana:		"support",
+	lucio:		"support",
+	zenyatta:	"support",
+	moira:		"support",
+	brigitte:	"support",
+	
+	dva:		"tank",
+	orisa:		"tank",
+	winston:	"tank",
+	zarya:		"tank",
+	roadhog:	"tank",
+	reinhardt:	"tank",
+	wrecking_ball:"tank",
+};
+
+var rank_names = [
+	"unraked",
+	"bronze",
+	"silver",
+	"gold",
+	"platinum",
+	"diamond",
+	"master",
+	"grandmaster",
+	"top500"
+];
